48c4c947eaaad245bd551231155b2a0782030523
erjok2020 <erjok2020@github.com>
2024-09-23 16:19:06 +0300

    task 3

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,12 +1,30 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development", // Set mode to development for the dev server
-  entry: "./js/dashboard_main.js", // Entry point for the application
+  mode: "development", // Set mode to development
+  devtool: "inline-source-map", // Set devtool to inline-source-map
+  entry: {
+    header: "./modules/header/header.js",
+    body: "./modules/body/body.js",
+    footer: "./modules/footer/footer.js",
+  },
   output: {
-    filename: "bundle.js", // Output filename
+    filename: "[name].bundle.js", // Output filename
     path: path.resolve(__dirname, "public"), // Output directory
     publicPath: "/", // Public URL of the output directory when referenced in a browser
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: "all", // Set chunks to all
+    },
   },
   devServer: {
     contentBase: path.join(__dirname, "public"), // Serve content from the public directory
